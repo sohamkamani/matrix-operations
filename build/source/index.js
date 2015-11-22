a78b3c7b0ed9b667017aa28feb8179d684090d57
sohamkamani <sohamkamani@gmail.com>
2015-11-22 10:49:49 +0530

    added build to .gitignore

--- build/source/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var add = require('./lib/add');
-
-module.exports = {
-  add: add
-};
--- build/source/validations/canAdd.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function (mat1, mat2) {
-  return mat1.dimensions.m === mat2.dimensions.m && mat1.dimensions.n === mat2.dimensions.n;
-};

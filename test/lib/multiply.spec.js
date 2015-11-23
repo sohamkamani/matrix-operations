3c35b1bd4b82bcbe79f1524492eff9ee6f289df6
sohamkamani <sohamkamani@gmail.com>
2015-11-23 19:03:31 +0530

    added multiply spec

--- test/lib/multiply.spec.js
+++ test/lib/multiply.spec.js
@@ -1,24 +1,29 @@
 'use strict';
 import Matrix from '../../source/Matrix';
 import {
-  assert
+  assert, expect
 }
 from 'chai';
 
 describe('Matrix multiplication', function () {
+  let m1 = Matrix([
+    [1],
+    [2],
+    [3]
+  ]);
+  let m2 = Matrix([
+    [1, 1, 1]
+  ]);
+  let m3 = Matrix([[5],[3]]);
   it('multiplies', () => {
-    let m1 = Matrix([
-      [1],
-      [2],
-      [3]
-    ]);
-    let m2 = Matrix([
-      [1, 1, 1]
-    ]);
     assert.deepEqual(m1.multiply(m2).value(), [
       [1, 1, 1],
       [2, 2, 2],
       [3, 3, 3]
     ]);
+  });
+
+  it('throws error if matrices cannot be multiplied', ()=>{
+    expect(()=>m1.multiply(m3)).to.throw(Error);
   });
 });
---
+++ test/lib/submatrix.spec.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('Sub- matrix', function(){
+  
+});

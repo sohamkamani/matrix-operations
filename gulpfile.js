a8eb943774d000f74bad2d4cbb13f51811d85304
sohamkamani <sohamkamani@gmail.com>
2015-12-03 17:16:26 +0530

    added source map support

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,39 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var babel = require('gulp-babel');
 var clean = require('gulp-clean');
+var istanbul = require('gulp-babel-istanbul');
 var sourcemaps = require('gulp-sourcemaps');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+// var istanbul = require('gulp-istanbul');
 var path = require('path');
+var isparta = require('isparta');
+var mergeStream = require('merge-stream');
+
+require('babel-core/register');
 
 var dirs = {
   source: 'source',
   dist: 'build',
   sourceRoot: path.join(__dirname, 'source'),
-  testRoot: path.join(__dirname, 'test')
+  testRoot: path.join(__dirname, 'test'),
+  testBuild : 'build/test',
+  sourceBuild : 'build/source'
  };
 
 var files = {
   test: 'test/**/*.js',
-  testBuild: dirs.testBuild + '/**/*.js',
-  source: dirs.source + '/**/*.js',
-  sourceBuild: dirs.sourceBuild + '/**/*.js',
+  testBuild: path.join(dirs.testBuild, '/**/*.js'),
+  source: path.join(dirs.source, '/**/*.js'),
+  sourceBuild: path.join(dirs.sourceBuild, '/**/*.js'),
   entryPoint: dirs.sourceBuild + '/index.js'
 };
 
 //Test tasks
 gulp.task('source:build', function () {
   return gulp.src(files.source)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
     .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015']
@@ -37,6 +49,8 @@ gulp.task('source:build', function () {
 
 gulp.task('test:build', function () {
   return gulp.src(files.test)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
     .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015']
@@ -51,12 +65,22 @@ gulp.task('test:build', function () {
 gulp.task('build:all', ['source:build', 'test:build']);
 
 
-gulp.task('test', ['build:all'], function () {
-  return gulp.src('build/test/**/*.js', {
+gulp.task('pre-test',['build:all'], function () {
+  return gulp.src([files.sourceBuild])
+    .pipe(istanbul({
+      instrumenter: isparta.Instrumenter
+    }))
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['build:all','pre-test'], function () {
+  return gulp.src(files.testBuild, {
       read: false
     })
-    .pipe(mocha());
+    .pipe(mocha())
+    .pipe(istanbul.writeReports());
 });
+
 
 gulp.task('clean:all', function () {
   return gulp.src('build', {
